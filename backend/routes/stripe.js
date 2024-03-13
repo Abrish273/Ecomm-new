4b016eaa6a6d028bce57f0e7825b477f4af2302b
Abrish273 <tilahuna13@gmail.com>
2024-03-13 10:06:25 +0300

    first commit

---
+++ backend/routes/stripe.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+router.post("/create-checkout-session", async (req, res) => {
+  const line_items = req.body.cartItem.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
+          }
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+  });
+
+  res.send({ url: session.url });
+});
+
+module.exports = router;
